929fb170bf70d3e19fc91eeb3acf07175dc834b6
Hudson Schumaker <hudson@.cp.br>
2016-07-19 18:51:26 -0300

    Frist

---
+++ Assets/Scripts/BladeMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BladeMovement : MonoBehaviour {
+
+	public int speed;
+	void Start () {
+		speed = 1;
+	}
+
+	void Update () {
+		transform.Rotate(0,speed*Time.deltaTime,0);
+		speed++;
+	}
+}
---
+++ Assets/Scripts/BladePunch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BladePunch : MonoBehaviour {
+
+
+	void OnCollisionEnter(Collision collision){
+
+		collision.gameObject.transform.TransformDirection (Vector3.forward * 1000000);
+	}
+
+}
